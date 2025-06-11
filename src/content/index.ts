d67b403e798a400e130930707eae4f51718529aa
alastair <garneralastair@gmail.com>
2025-06-11 21:18:39 +0100

    Run prettier

--- src/content/index.ts
+++ src/content/index.ts
@@ -6,29 +6,29 @@ type Project = {
 
 export const PROJECTS: Project[] = [
   {
-    path: "coach",
-    title: "Hello Kat ",
+    path: 'coach',
+    title: 'Hello Kat ',
     description:
-      "The design and experimental evaluation of a mobile application for exercise habit formation using behavioural changes techniques"
+      'The design and experimental evaluation of a mobile application for exercise habit formation using behavioural changes techniques',
   },
   {
-    path: "two",
-    title: "Project Two",
+    path: 'two',
+    title: 'Project Two',
     description:
-      "The design and experimental evaluation of a mobile application for exercise habit formation using behavioural changes techniques"
+      'The design and experimental evaluation of a mobile application for exercise habit formation using behavioural changes techniques',
   },
   {
-    path: "three",
-    title: "Project Three",
+    path: 'three',
+    title: 'Project Three',
     description:
-      "The design and experimental evaluation of a mobile application for exercise habit formation using behavioural changes techniques"
+      'The design and experimental evaluation of a mobile application for exercise habit formation using behavioural changes techniques',
   },
   {
-    path: "four",
-    title: "Project Four",
+    path: 'four',
+    title: 'Project Four',
     description:
-      "The design and experimental evaluation of a mobile application for exercise habit formation using behavioural changes techniques"
-  }
+      'The design and experimental evaluation of a mobile application for exercise habit formation using behavioural changes techniques',
+  },
 ];
 
 export type TimelineElement = {
@@ -39,23 +39,23 @@ export type TimelineElement = {
 
 export const TIMELINE: TimelineElement[] = [
   {
-    title: "BSc Psychology",
-    description: "Wilfrid Laurier University, Canada",
-    date: "2013"
+    title: 'BSc Psychology',
+    description: 'Wilfrid Laurier University, Canada',
+    date: '2013',
   },
   {
-    title: "Started First Job",
-    description: "Began working as a UX researcher.",
-    date: "2014"
+    title: 'Started First Job',
+    description: 'Began working as a UX researcher.',
+    date: '2014',
   },
   {
-    title: "Promoted to Senior UX Researcher",
-    description: "Advanced to a senior position, leading research projects.",
-    date: "2015"
+    title: 'Promoted to Senior UX Researcher',
+    description: 'Advanced to a senior position, leading research projects.',
+    date: '2015',
   },
   {
-    title: "Not quite yet",
-    description: "Took on a leadership role in the UX design team.",
-    date: "2016"
-  }
+    title: 'Not quite yet',
+    description: 'Took on a leadership role in the UX design team.',
+    date: '2016',
+  },
 ];
