f6472ee1d7e2f88bff7314d0307b58aecac6fc25
alastair <garneralastair@gmail.com>
2025-11-11 20:48:38 +0000

    Add import sorting (not working yet)

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,8 +2,24 @@ const config = {
   singleQuote: true,
   trailingComma: 'all',
   printWidth: 80,
-  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
   tailwindFunctions: ['clsx'],
+  // https://github.com/trivago/prettier-plugin-sort-imports/tree/main?tab=readme-ov-file#apis
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+    '^astro/(.*)$',
+    '^[./]',
+    '^@(assets|images|public)/(.*)$',
+  ],
+  // Force a newline between import groups
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  // Do not auto-sort side-effect imports, i.e. `import 'side-effect-lib'`
+  importOrderSideEffects: false,
+  plugins: [
+    'prettier-plugin-astro',
+    '@trivago/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
 };
 
 export default config;
